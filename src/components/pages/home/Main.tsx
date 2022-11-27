68109b0f9c5755fb051f21d2358097a40723ce5a
Jakub <jkbmielewczyk@gmail.com>
2022-11-27 13:23:39 +0100

    Moved start layer from Main to new component & to new styled component

--- src/components/pages/home/Main.tsx
+++ src/components/pages/home/Main.tsx
@@ -1,37 +1,31 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 
-import FeaturedProduct from './FeaturedProduct'
+import StartBackground from './StartBackground'
+import HeaderMain from './HeaderMain'
 
+import { StyledStartBackground } from '../../styles/StartBackground.styled'
 import { StyledMain } from '../../styles/Main.styled'
 import { StyledMainHeader } from '../../styles/MainHeader.styled'
+
 
 import { keyboards } from '../../../data/keyboards';
 
 const Main = () => {
-    const [started, setStarted] = useState(false)
-
+    
     const keyboardsList = keyboards.map(item =>(
     <Link key={item.name} to={`product/keyboards/${item.name}`}>
     <div className="product">
         <p className="shoeTitle">{item.name}</p>
     </div>
     </Link>))
-
-    const handleStarted = () => {
-        setStarted(prevValue => !prevValue)
-    }
     return (
         <StyledMain>
-            <div className={started ? "startBackground active" : "startBackground"}>
-            </div>
-            <div className={started ? 'startLayout active' : 'startLayout'}>
-                <h1 className='company-name'>keyboards</h1>
-                <p className="startText">Find your best clickers</p>
-                <button onClick={handleStarted} className='btnGetStarted'>Enter<p className='enter-symbol'>⮐</p></button>
-            </div>
+            <StyledStartBackground>
+                <StartBackground/>
+            </StyledStartBackground>
             <StyledMainHeader>
-                <FeaturedProduct/>
+                <HeaderMain/>
             </StyledMainHeader>
             {keyboardsList}
         </StyledMain>
--- src/components/styles/Main.styled.js
+++ src/components/styles/Main.styled.js
@@ -1,92 +1,10 @@
 import styled from 'styled-components'
 
-import backgroundImage from '../../images/startBackground/start-background-light.jpg'
-
 export const StyledMain = styled.main`
 width: 100%;
 height: calc(100vh - 60px); // 60px - Navigation height
 border: 1px solid black;
-.startBackground {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background-image: url(${backgroundImage});
-    background-repeat: no-repeat; 
-    background-size: 100vh;
-    z-index: 2;
-    transition: .3s;
-    &.active {
-        transform: translateX(100vw);
-    }
-}
-.startLayout {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    z-index: 3;
-    transition: .3s;
-    &.active {
-        transform: translateX(100vw);
-    }
-    .company-name {
-        position: absolute;
-        top: 15px;
-        left: 15px;
-    }
-    .startText {
-        position: absolute;
-        bottom: 30vh;
-        left: 50vw;
-        transform: translateX(-50%);
-        width: 100%;
-        text-align: center;
-        font-size: 24px;
-        font-weight:700;
-        
-    }
-    .btnGetStarted {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        bottom: 15vh;
-        left: 50vw;
-        transform: translateX(-50%);
-        width: 150px;
-        height: 60px;
-        min-width: 120px;
-        background-color: #E0E0D1;
-        color: grey;
-        border: none;
-        border-radius: 10px;
-        font-size: 16px;
-        font-weight: 700;
-        border-right: 5px solid #BDBDAF;
-        border-bottom: 5px solid #CCCCBE;
-        border-left: 5px solid #ADADA1;
-        border-top: 5px solid #ADADA1;
-        box-shadow: 0 0 7px black;
-        transition: .1s;
-        .enter-symbol {
-            font-size: 22px;
-            position: absolute;
-            top: 56%;
-            left: 65%;
-            transform: translateY(-50%);
-        }
-    }
-    .btnGetStarted:active {
-        transform: translateX(-50%) scale(0.95);
-        box-shadow: 0 0 7px 1px black;
-        .enter-symbol {
-            transform: translateY(-49.6%);
-        }
-        }
-}
+
 .product {
     width: 100vw;
     height: 30vh;
