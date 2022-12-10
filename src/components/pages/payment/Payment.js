100743e28e5d1c0cacb23c753ad2f0d86beba385
Jakub Mielewczyk <jkbmielewczyk@gmail.com>
2022-12-10 15:14:16 +0100

    Add elements & styling to Payment component

--- src/components/pages/payment/Payment.js
+++ src/components/pages/payment/Payment.js
@@ -5,7 +5,37 @@ import { StyledPayment } from '../../styles/Payment.styled';
 const Payment = () => {
     return (
     <StyledPayment>
-        <div>Payment</div>
+        <div className='payment-wrapper'>
+            <p className='payment-details-txt' >Payment Details</p>
+            <form className='form' >
+                <label htmlFor='name-on-card'>
+                    Name on card
+                    <input autoComplete='off' id='name-on-card' type='text'></input>
+                </label>
+                <label htmlFor='card-number' >
+                    Card number
+                    <input autoComplete='off' id='card-number' type='number'></input>
+                </label>
+                <div className='container'>
+                    <label>
+                        Expiration
+                        <div className='expiration-labels-container'>
+                            <input autoComplete='off' type='number'></input>
+                            <p className='slash'>/</p>
+                            <input autoComplete='off' type='number'></input>
+                        </div>   
+                    </label>
+                    <label htmlFor='card-cvc'>
+                        CVC
+                        <input autoComplete='off' id='card-cvc' type='number'></input>
+                    </label>
+                </div>
+                <div className='container' >
+                    <button className='cancel-button'>Cancel order</button>
+                    <input className='submit-button' type='submit' value='Complete order'></input>
+                </div>
+            </form>
+        </div>
     </StyledPayment>
    ) 
 }
--- src/components/styles/Payment.styled.js
+++ src/components/styles/Payment.styled.js
@@ -1,6 +1,70 @@
 import styled from 'styled-components'
 
 export const StyledPayment = styled.main`
+display: flex;
 width: 100%;
+height: 100%;
 flex-grow: 1;
+padding: 20px;
+.payment-wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    border-radius: 8px;
+    padding: 20px;
+    gap: 20px;
+    background-color: #EDF2F7;
+    .payment-details-txt {
+        font-size: 24px;
+    }
+    .form {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        flex-grow: 1;
+        .container {
+            display: flex;
+            gap: 10px;
+        }
+        label {
+            display: flex;
+            flex-direction: column;
+            input{
+                height: 50px;
+                border: none;
+                border-radius: 8px;
+            }
+        }
+        .expiration-labels-container {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: 5px;
+            .slash {
+                color: grey;
+                font-size: 32px;
+                width: 20px;
+            }
+        }
+        .cancel-button, .submit-button {
+            font-size: 16px;
+            width: 100%;
+            height: 50px;
+            border-radius: 8px;
+        }
+        .submit-button {
+            border: none;
+            background-color: #4299E1;
+            color: white;
+        }
+        .cancel-button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-decoration: none; 
+            color: black;
+            border: 1px solid black;
+        }
+    }
+}
 `
