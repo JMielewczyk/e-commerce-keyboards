48c8700d7d7063ecf9359df6db137def53d901e6
Jakub Mielewczyk <jkbmielewczyk@gmail.com>
2022-12-19 19:38:38 +0100

    Rewrote change case in reducer to not mutate state.

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,31 +1,35 @@
 export function manageBasket(state, action) {
-    console.log(state)
-    switch(action.type) {
-        case 'add': {
-            return [...state, {
-                name: action.payload.name,
-                quantity: action.payload.quantity,
-                price: action.payload.price,
-                image: action.payload.image,
-                path: action.payload.pathToProduct,
-            }]  
+  switch (action.type) {
+    case 'add': {
+      return [
+        ...state,
+        {
+          name: action.payload.name,
+          quantity: action.payload.quantity,
+          price: action.payload.price,
+          image: action.payload.image,
+          path: action.payload.pathToProduct
         }
-        case 'change': {
-            return state.map(element => {
-                if (element.name === action.payload.name) {
-                    element.quantity += action.payload.quantity
-                    element.price = element.quantity * action.payload.price
-                    return element
-                } else {
-                    return element
-                }
-            })
-        }
-        case 'delete': {
-            const filteredState = state.filter(element => element.name !== action.payload.name)
-            return [...filteredState]
-        }
-        default:
-            return state
+      ];
     }
+    case 'change': {
+      return state.map((element) => {
+        if (element.name === action.payload.name) {
+          const newQuantity = element.quantity + action.payload.quantity;
+          console.log(state);
+          return {
+            ...element,
+            quantity: newQuantity,
+            price: newQuantity * action.payload.price
+          };
+        } else return { ...element };
+      });
+    }
+    case 'delete': {
+      const filteredState = state.filter((element) => element.name !== action.payload.name);
+      return [...filteredState];
+    }
+    default:
+      return state;
+  }
 }
