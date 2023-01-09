8e7598b16a4d51a90bb6ac9e5555202a5c8dca68
Jakub Mielewczyk <jkbmielewczyk@gmail.com>
2023-01-09 15:38:07 +0100

    Add links to images in data.json

--- src/pages/product/functions/handleAmount.js
+++ src/pages/product/functions/handleAmount.js
@@ -1,7 +1,7 @@
 export const handleAmount = (orderAmount, setOrderAmount, value) => {
-  if (value === "plus") {
+  if (value === 'plus') {
     setOrderAmount((prevValue) => prevValue + 1);
-  } else if (value === "minus") {
+  } else if (value === 'minus') {
     if (orderAmount === 0) return;
     setOrderAmount((prevValue) => prevValue - 1);
   }
